54f32730055cc8728cfa6a4390d1e0dbaef1ae12
Makarona10 <ahmedneyssi@gmail.com>
2024-08-02 01:38:39 +0300

    Task 7 done

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,6 @@ from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple:
+    '''Returns a tuple contains the first arg and the second's
+    square as a float'''
     return (tuple((k, float(v * v))))
