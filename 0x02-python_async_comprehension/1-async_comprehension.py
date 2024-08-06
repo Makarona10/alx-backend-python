8320f6c027b14708766f5d6c4d7757f55d5be4ae
Makarona10 <ahmedneyssi@gmail.com>
2024-08-07 02:53:41 +0300

    Task 1 done

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> Generator[float, None, None]:
-    async for i in async_generator():
-        yield i
+    return [i async for i in async_generator()]
