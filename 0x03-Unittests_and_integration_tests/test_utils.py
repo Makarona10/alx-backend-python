202e0de6f512570a1e533b8130e208f0ccb855e1
Makarona10 <ahmedneyssi@gmail.com>
2024-08-24 06:11:09 +0300

    Task 1 done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,13 +4,26 @@
 from utils import access_nested_map
 import unittest
 from parameterized import parameterized
+from typing import Mapping, Sequence
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    @parameterized.expand([({"a": 1}, ["a"], 1),
-                           ({"a": {"b": 1}}, ["a"], {"b": 1}),
-                           ({"a": {"b": 2}}, ["a", "b"], 2)
+    @parameterized.expand([({"a": 1}, ("a",), 1),
+                           ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                           ({"a": {"b": 2}}, ("a", "b"), 2)
                            ])
-    def test_access_nested_map(self, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
+        
+    @parameterized.expand([({}, ("a",)),
+                           ({"a": 1}, ("a", "b"))])
+    def test_access_nested_map_exception(self, nested_map: Mapping, key: Sequence) -> None:
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, key)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
