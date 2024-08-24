f501cd16a78535efbd3715878560310b15225378
Makarona10 <ahmedneyssi@gmail.com>
2024-08-25 00:25:07 +0300

    Task 2 done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """0x03. Unittests and Integration Tests"""
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 from typing import Mapping, Sequence
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -14,16 +16,26 @@ class TestAccessNestedMap(unittest.TestCase):
                            ])
     def test_access_nested_map(self, nested_map: Mapping,
                                path: Sequence, expected: int) -> None:
+        """unit test for utils.access_nested_map"""
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
         
     @parameterized.expand([({}, ("a",)),
                            ({"a": 1}, ("a", "b"))])
-    def test_access_nested_map_exception(self, nested_map: Mapping, key: Sequence) -> None:
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence) -> None:
+        """test that a KeyError is raised for the specific inputs"""
         with self.assertRaises(KeyError):
-            access_nested_map(nested_map, key)
+            access_nested_map(nested_map, path)
 
 
-
-if __name__ == '__main__':
-    unittest.main()
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get_json):
+        mock_get_json.return_value.json.return_value = test_payload
+        res = get_json(test_url)
+        self.assertEqual(res, test_payload)
+        mock_get_json.assert_called_once_with(test_url)
